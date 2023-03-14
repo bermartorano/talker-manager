78c01243cc1f5e63fe177a67dd4d564ff05354c7
Bernardo Martorano <bernardormartorano@gmail.com>
2023-03-14 19:57:33 -0300

    tentantica req 03

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const talkerRouter = require('./talker.router');
+const loginRouter = require('./login.router');
 
 const router = express.Router();
 
-const talkerRouter = require('./talker.router');
-
 router.use(talkerRouter);
+router.use(loginRouter);
 
 module.exports = router;
---
+++ src/routers/login.router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+// const { readTalkersFile } = require('../utils/readAndWrite');
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+  const { body: { email, password } } = req;
+  const randomNumber = Math.floor(Math.random() * (10 ** 16)) + 1;
+  const randomToken = JSON.stringify(randomNumber);
+
+  res.status(200).json({ token: randomToken });
+});
+
+module.exports = router;
